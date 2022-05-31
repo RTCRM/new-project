1b67796967d139ddd39bb67f0972aae0baeabc7f
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-05-31 16:09:16 -0400

    new update

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import './style.css'
 const app = document.querySelector<HTMLDivElement>('#app')!
 
 app.innerHTML = `
-  <h1>My New Project</h1>  
+  <h1>My New Project</h1> 
+  <p>New changes!</p> 
 `
